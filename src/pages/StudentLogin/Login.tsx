9e2806c8090b29fb2b076c2455bf30a6222f13ab
TABRILLA <angelestabrilla10@gmail.com>
2023-11-15 20:29:43 +0800

    new log in

--- src/pages/StudentLogin/Login.tsx
+++ src/pages/StudentLogin/Login.tsx
@@ -7,35 +7,40 @@ const Login = () => {
   return (
     <section>
         {/* hidden lg:block lg:w-1/2 bg-cover */}
-            <div className="flex flex-wrap content-center justify-center w-full min-h-screen py-10" 
-                style={{
-                    backgroundImage: `url(${IMGS.STUDENTBACK})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    backgroundRepeat: 'no-repeat',
-                }}>
+    
+        <div className="bg-white">
+            <div className="flex justify-center h-screen">
+                    <div className="hidden bg-cover lg:block lg:w-2/3" 
+                        style={{ backgroundImage: `url(${IMGS.STUDENTBACK})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', }}>
 
-                <div className="flex shadow-md">
-
-                    <div className="flex flex-wrap content-center justify-center rounded-r-md" style={{width: '24rem', height: '32rem'}}>
-                        <img className="hidden lg:block lg:w-full rounded-r-md" src={IMGS.STILOGO} />
-                    </div>
-
-                    <div className="flex flex-wrap content-center justify-center bg-white bg-opacity-75 rounded-l-md" style={{width: '24rem', height: '32rem'}}>
-                        <div className="w-72">
-                            <h1 className="py-10 text-3xl font-bold text-center">WELCOME STIERS</h1>
-
-                                <div className="space-y-5">
-                                    <Input label="Student ID Number" color="black" crossOrigin={undefined} />
-
-                                    <Input label="Password" type="password" color="black" crossOrigin={undefined} />
-
-                                    <Button fullWidth>Sign In</Button>                             
-                                </div>
+                        <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+                            <div>
+                                <h2 className="text-4xl font-bold text-white mt-20">Welcome Stier</h2>
+                                <p className="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus molestiae</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+
+                    <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+                        <div className="flex-1">
+                            <div className="text-center">
+                                <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white">STI</h2>
+                                <p className="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+                            </div>
+
+                            <div className="mt-8">
+                                <div className="space-y-5">     
+                                    <Input label="Student ID Number" color="black" crossOrigin={undefined} />
+                                    <Input label="Password" type="password" color="black" crossOrigin={undefined} />                                  
+                                    {/* <h1 className="text-sm text-gray-400 focus:text-blue-500 hover:text-red-500 hover:underline">Forgot password?</h1> */}
+                                    <Button fullWidth>Sign In</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
+        </div>
+
 
     </section>
   )

2b1669697406defc2cd3ab43fddbf4a3ede2dddd
TABRILLA <angelestabrilla10@gmail.com>
2023-11-18 10:35:52 +0800

    i

--- src/pages/Main/StudentMain.tsx
+++ src/pages/Main/StudentMain.tsx
@@ -1,6 +1,29 @@
+import IMGS from "../../constants/IMGS"
+import { Button } from "@material-tailwind/react"
+import { useNavigate } from "react-router-dom";
+
 const StudentMain = () => {
+
+  const navigate = useNavigate();
+
+  const handleNavigateLogout = () => {
+    navigate("/");
+  };
+
   return (
-    <div>StudentMain</div>
+    <section className="flex justify-center h-screen"  style={{
+      backgroundImage: `url(${IMGS.STUDENTBACK})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}>
+      
+      <div>
+        <Button onClick={handleNavigateLogout} className='mx-auto'>LOGOUT</Button>
+      </div>
+
+    </section>
+    
   )
 }
 
